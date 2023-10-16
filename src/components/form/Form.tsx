69e2b185c128d0b0b70e44fa67bf68bf6f48f30a
Bernard <browb130@mail.broward.edu>
2023-10-16 02:45:47 -0400

    added more styling to checkout form along with proper state for inputs.

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,10 +1,32 @@
 // components
+import { useState } from 'react'
 import { FormInput } from './FormInput'
 
+type input = {
+    [name: string]: string
+}
+
 export const Form = () => {
+    const [inputs, setInput] = useState<input>({})
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(inputs)
+    }
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInput((prevInput) => {
+            return { ...prevInput, [name]: value }
+        })
+    }
+    console.log(inputs)
+
     return (
-        <form className="px-6 md:px-7 xl:px-12">
-            <h2 className="mt-6 text-subHeader tracking-[1px] md:mt-[30px] md:text-desktopSubheader xl:mt-[54px]">
+        <form
+            onSubmit={handleSubmit}
+            className="rounded-lg bg-white p-[24px_24px_30px] shadow-lg md:p-[30px_27px] xl:p-[54px_48px_48px]"
+        >
+            <h2 className="text-subHeader tracking-[1px]  md:text-desktopSubheader">
                 CHECKOUT
             </h2>
             <div className="mt-8 md:mt-10">
@@ -12,9 +34,67 @@ export const Form = () => {
                     BILLING DETAILS
                 </h3>
                 <div className="mt-4 grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2">
-                    <FormInput label="Name" placeholder="username" />
-                    <FormInput label="Email Address" placeholder="username" />
-                    <FormInput label="Phone Number" placeholder="username" />
+                    <FormInput
+                        name="name"
+                        label="Name"
+                        placeholder="Ken Williams"
+                        handleChange={handleChange}
+                        value={inputs.name || ''}
+                    />
+                    <FormInput
+                        name="email"
+                        label="Email Address"
+                        placeholder="kenwill@mail.com"
+                        handleChange={handleChange}
+                        value={inputs.email || ''}
+                    />
+                    <FormInput
+                        name="number"
+                        label="Phone Number"
+                        placeholder="+1 786-222-0123"
+                        handleChange={handleChange}
+                        value={inputs.number || ''}
+                    />
+                    {/* <FormInput label="Email Address" placeholder="username" /> */}
+                    {/* <FormInput label="Phone Number" placeholder="username" /> */}
+                </div>
+            </div>
+            <div className="mt-8 md:mt-10">
+                <h3 className="text-link tracking-[0.929px] text-nude-200">
+                    SHIPPING INFO
+                </h3>
+                <div className="mt-4 grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2">
+                    <FormInput
+                        className="md:col-[span_2]"
+                        name="address"
+                        label="Your Address"
+                        placeholder="1800 Mcnab Avenue"
+                        handleChange={handleChange}
+                        value={inputs.address || ''}
+                    />
+                    <FormInput
+                        name="zip"
+                        label="ZIP Code"
+                        placeholder="32382"
+                        handleChange={handleChange}
+                        value={inputs.zip || ''}
+                    />
+                    <FormInput
+                        name="city"
+                        label="City"
+                        placeholder="Delray"
+                        handleChange={handleChange}
+                        value={inputs.city || ''}
+                    />
+                    <FormInput
+                        name="country"
+                        label="Country"
+                        placeholder="United States"
+                        handleChange={handleChange}
+                        value={inputs.country || ''}
+                    />
+                    {/* <FormInput label="Email Address" placeholder="username" /> */}
+                    {/* <FormInput label="Phone Number" placeholder="username" /> */}
                 </div>
             </div>
         </form>
--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,14 +1,28 @@
 interface FormInputProps {
+    className?: string
     placeholder: string
     label: string
+    name: string
+    value: string
+    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const FormInput = ({ placeholder, label }: FormInputProps) => {
+export const FormInput = ({
+    className,
+    placeholder,
+    label,
+    handleChange,
+    name,
+    value,
+}: FormInputProps) => {
     return (
-        <div className="flex flex-col">
+        <div className={`flex flex-col ${className}`}>
             <label className="text-label">{label}</label>
             <input
+                value={value}
+                name={name}
                 className="mt-[9px] rounded-lg border border-[#CFCFCF] px-6 py-[18px] text-formInput placeholder:opacity-60 focus:border"
+                onChange={handleChange}
                 placeholder={placeholder}
                 type="text"
             />
